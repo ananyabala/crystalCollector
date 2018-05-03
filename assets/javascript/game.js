859bf3a311e9bbe0ea6923b5ea2bf8c364493231
Ananya Bala <ananyabala42@gmail.com>
2018-05-03 17:59:45 -0400

    working app

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,99 +12,122 @@ Planning:
 - We need to reset the game back to 0 once it goes over the target score
 */
 
-// - Each crystal needs to have a number between 1-12 randomly allocated to it
+// GLOBAL VARIABLES
 var crystal1;
 var crystal2;
 var crystal3;
 var crystal4;
 var storeScore = [];
-// Game Counters
-var totalScore=0;
-var winCounter=0;
-var lossCounter=0;
+var crystalScore;
+var targetScore;
 
+// Game Counters
+var winCounter = 0;
+var lossCounter = 0;
+var totalScore = 0;
+
+// $("#total").innerHTML=totalScore;
+
+ // - Each crystal needs to have a number between 1-12 randomly allocated to it
+crystal1 = Math.floor(Math.random() * 12) + 1;
+crystal2 = Math.floor(Math.random() * 12) + 1;
+crystal3 = Math.floor(Math.random() * 12) + 1;
+crystal4 = Math.floor(Math.random() * 12) + 1;
+console.log(crystal1);
+console.log(crystal2);
+console.log(crystal3);
+console.log(crystal4);
+
+// CALLING FUNCTIONS
+startGame();
+roundComplete();
+// FUNCTION CODE
 
 function startGame() {
-    // - The target score needs to have a number between 19-120 randomly allocated to it
+    totalScore=0;
     document.getElementById("total").innerHTML = totalScore;
-    var targetScore = Math.floor(Math.random() * 101) + 19;
+    // - The target score needs to have a number between 19-120 randomly allocated to it
+    targetScore = Math.floor(Math.random() * 101) + 19;
     console.log(targetScore);
-
-    
-
-    // Generate random numbers for each crystal and print them into the console
-    crystal1 = Math.floor(Math.random() * 12) + 1;
-    crystal2 = Math.floor(Math.random() * 12) + 1;
-    crystal3 = Math.floor(Math.random() * 12) + 1;
-    crystal4 = Math.floor(Math.random() * 12) + 1;
-    console.log(crystal1);
-    console.log(crystal2);
-    console.log(crystal3);
-    console.log(crystal4);
-
     // - We need to print the target score onto the page
     document.getElementById("targetScore").innerHTML = targetScore;
 
-// - ONCLICK FOR CRYSTAL IMAGES
+    // - ONCLICK FOR CRYSTAL IMAGES
     // Store HTML element in variable
     var crystalClick1 = document.getElementById("crystal1");
     // Add event listener
     crystalClick1.onclick = function () {
         console.log(crystal1);
+        totalScore= totalScore + crystal1;
+        document.getElementById("total").innerHTML=totalScore;
+        roundComplete();
     }
+
     var crystalClick2 = document.getElementById("crystal2");
     crystalClick2.onclick = function () {
         console.log(crystal2);
+        totalScore= totalScore + crystal2;
+        document.getElementById("total").innerHTML=totalScore;
+        roundComplete();
     }
     var crystalClick3 = document.getElementById("crystal3");
-    
     crystalClick3.onclick = function () {
         console.log(crystal3);
+        totalScore= totalScore + crystal3;
+        document.getElementById("total").innerHTML=totalScore;
+        roundComplete();
 
     }
     var crystalClick4 = document.getElementById("crystal4");
     crystalClick4.onclick = function () {
         console.log(crystal4);
+        totalScore= totalScore + crystal4;
+        document.getElementById("total").innerHTML=totalScore;
+        roundComplete();
     }
 
+    
+
+
 }
-startGame();
 
 function roundComplete() {
 
     // First, log an initial status update in the console
     // telling us how many wins, losses, and guesses are left.
-    console.log("WinCount: " + winCounter + " | LossCount: " + lossCounter );
-  
+    console.log("WinCount: " + winCounter + " | LossCount: " + lossCounter);
+    console.log("In Round complete");
     // HTML UPDATES
     // If the targetScore is equal to the totalScore (we are able to calculate the number right)
+    console.log(targetScore);
+    console.log(totalScore);
     if (targetScore === totalScore) {
-  
-      // Add to the win counter
-      winCounter++;
-  
-      // Update the win counter in the HTML
-      document.getElementById("win-counter").innerHTML = winCounter;
-  
-      // Restart the game
-      startGame();
+
+        // Add to the win counter
+        winCounter++;
+
+        // Update the win counter in the HTML
+        document.getElementById("win-counter").innerHTML = winCounter;
+
+        // Restart the game
+        startGame();
     }
-  
+
     // If totalScore is greater than targetScore (You've lost the game!)
-    else if (targetScore<totalScore) {
-  
-      // Add to the loss counter
-      lossCounter++;
-  
-      // Update the loss counter in the HTML
-      document.getElementById("loss-counter").innerHTML = lossCounter;
-  
-      // Restart the game
-      startGame();
-  
+    else if (targetScore < totalScore) {
+
+        // Add to the loss counter
+        lossCounter++;
+
+        // Update the loss counter in the HTML
+        document.getElementById("loss-counter").innerHTML = lossCounter;
+
+        // Restart the game
+        startGame();
+
     }
-  
-  }
+
+}
 
 
 
